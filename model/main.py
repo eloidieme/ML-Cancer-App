b0625b0f973259aa5e6f8853b5ddf69d632a45e9
Eloi Dieme <eloidieme@gmail.com>
2024-02-21 15:37:35 +0100

    implemented and generated model and scaler pickle files

--- model/main.py
+++ model/main.py
@@ -1,49 +1,50 @@
 import pandas as pd
-from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
 
-class ColumnProcessor(TransformerMixin):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return X.drop(['Unnamed: 32'], axis=1)
-
-def train_model(X, y):
-    pass
-
-def preprocess_data():
-    data = pd.read_csv("data/data.csv")
+def create_model(data):
     X = data.drop(['diagnosis'], axis=1)
     y = data['diagnosis']
-    X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    X_pipeline = Pipeline([
-        ('column_processor', ColumnProcessor()),
-        ('imputer', SimpleImputer(strategy='median')),
-        ('std_scaler', StandardScaler())
-    ])
-    y_pipeline = Pipeline([
-        ('label_encoder', LabelEncoder())
-    ])
 
-    X_train = X_pipeline.fit_transform(X_train)
-    X_test = X_pipeline.transform(X_test)
-    y_train = y_pipeline.fit_transform(y_train)
-    y_test = y_pipeline.transform(y_test)
+    # scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-    return X_train, y_train, X_test, y_test
+    # split the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # train the model
+    model = LogisticRegression(penalty='l2', C=2.78)
+    model.fit(X_train, y_train)
+
+    # test the model
+    y_pred = model.predict(X_test)
+    print('Accuracy of our model:', accuracy_score(y_test, y_pred))
+    print('Classification report:\n', classification_report(y_test, y_pred))
+
+    return model, scaler
+
+def get_clean_data():
+    data = pd.read_csv("data/data.csv")
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+
+    return data
 
 def main():
-    X_train, y_train, X_test, y_test = preprocess_data()
+    data = get_clean_data()
 
-    model = train_model(X_train, y_train)
+    model, scaler = create_model(data)
+
+    with open('model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    with open('model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
 
 if __name__ == '__main__':
     main()
